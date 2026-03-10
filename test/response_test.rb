ceee826db861b423ac7779dff86b8d5d902a41ad
DiDi035 <baodihuynh@gmail.com>
2026-03-11 05:18:21 +0700

    fix rubocop issues

--- test/response_test.rb
+++ test/response_test.rb
@@ -2,16 +2,14 @@
 
 require 'test_helper'
 
-class ResponseSuccessTest < Minitest::Test
+class ResponseTest < Minitest::Test
   def test_success
     response = DiacritiX::Response::Success.new(3)
 
     assert response.success?
     refute response.failure?
   end
-end
 
-class ResponseFailureTest < Minitest::Test
   def test_failure
     response = DiacritiX::Response::Failure.new('error')
 
